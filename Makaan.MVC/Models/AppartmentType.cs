88e2e4b59f622a7ea2c2b1717234a5c5b7a0d6cf
MohammedHamed12121 <mh9182731122@gmail.com>
2024-02-06 19:43:40 +0200

    Initial Create

---
+++ Makaan.MVC/Models/AppartmentType.cs
@@ -0,0 +1,7 @@
+namespace Makaan.MVC.Models
+{
+    public enum AppartmentType
+    {
+        
+    }
+}
---
+++ Makaan.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Makaan.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
